083dce7e58e98e5b684a6eab3dc333f03c351820
Maryna Tsuk <73252923+marynatsuk@users.noreply.github.com>
2023-04-25 16:36:20 +0300

    Update and rename consDFS.py to sequential_dfs.py

--- sequential_dfs.py
+++ sequential_dfs.py
@@ -1,15 +1,17 @@
+import time
+
+
 def dfs(adj_list, start_node):
     visited = set()
-    visited_order = []
     stack = [start_node]
 
     while stack:
         current_node = stack.pop()
-        if current_node not in visited_order:
+        if current_node not in visited:
             visited.add(current_node)
-            visited_order.append(current_node)
             for neighbour in adj_list[current_node]:
-                if neighbour not in visited_order:
+                if neighbour not in visited:
                     stack.append(neighbour)
+                    time.sleep(0.1)
 
-    return visited, visited_order
+    return visited
