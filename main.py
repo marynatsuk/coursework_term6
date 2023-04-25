b2e8d11515a671b01d86f5218d0db12c414d40b4
Maryna Tsuk <73252923+marynatsuk@users.noreply.github.com>
2023-04-25 16:34:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,89 +1,57 @@
+import pandas as pd
 import time
 
-import pandas as pd
+import create_tree, sequential_dfs, parallel_dfs
 
-import createGraph, createTree, consDFS
-import random
+excel_path = 'C:/Users/serg/PycharmProjects/tpo_coursework_final/results.xlsx'
+balanced_json_path = 'C:/Users/serg/PycharmProjects/tpo_coursework_final/balanced_list_json/'
 
-import dfs_balanced
-import par_dfs
-import parallelDFS
-
-unb_path = 'C:/Users/serg/PycharmProjects/coursework_parallel/unbalanced_adj_list/'
-bal_path = 'C:/Users/serg/PycharmProjects/coursework_parallel/balanced_adj_list/'
-excel_path = 'C:/Users/serg/PycharmProjects/coursework_parallel/results.xlsx'
-
-NUM_TESTS = 4
+NUM_TESTS = 1
 sequential_time = []
 parallel_time = []
+visited = []
+
 
 def time_results(sequential_time_results, parallel_time_results):
     average_sequential = round(sum(sequential_time_results) / NUM_TESTS, 4)
     average_parallel = round(sum(parallel_time_results) / NUM_TESTS, 4)
-    print(f'Sequential DFS: {average_sequential} sec')
-    print(f'Parallel DFS: {average_parallel} sec')
-    print(f'Parallel is faster {average_sequential / average_parallel} times')
+    # print(f'Sequential DFS: {average_sequential} sec')
+    # print(f'Parallel DFS: {average_parallel} sec')
+    # print(f'Parallel is faster {average_sequential / average_parallel} times')
     return average_sequential, average_parallel
 
-if __name__ == '__main__':
-    #createGraph.create_directed_graph(20)
-    #createGraph.create_undirected_graph(10, 20)
-    # children = [2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 6, 6, 6]
-    # height = [2, 3, 3, 5, 7, 3, 5, 7, 9, 4, 6, 8, 4, 6, 7]
-    # for i in range(len(children)):
-    #     adj_list = createTree.create_balanced_tree(children[i], height[i])
 
-    #number = [7, 15, 40, 85, 364, 781, 1365, 1555, 3280, 19531, 21845, 349525, 488281]
-    number = [40, 1555, 3280, 19531, 21845, 349525, 488281]
-    #number = [50, 200, 500, 1000, 1500, 3000, 5000, 7500, 10000, 12500, 15000, 20000, 50000]
+if __name__ == '__main__':
+    # children = [4, 5, 3, 4, 3, 4, 5, 3, 4]
+    # height = [2, 2, 3, 3, 4, 4, 4, 5, 5]
+    # for i in range(len(children)):
+    #     adj_list = create_tree.create_balanced_tree(children[i], height[i])
+    #     print(len(adj_list))
+
+    number = [31, 40, 85, 121, 341, 781, 364, 1365]
     df = pd.DataFrame(columns=['# Nodes', 'Sequential time', 'Parallel time', 'Comparison'])
 
-    # for i in number:
-    #     adj_list = createTree.create_random_tree(i)
-
     for i in number:
+        # call adjacency list from file by path
+        path = balanced_json_path + str(i) + '_nodes.json'
+        adj_list = create_tree.read_json(path)
+        num_branches = len(adj_list[0])
         for j in range(NUM_TESTS):
-            #call adjacency list from file by path
-            path = bal_path + str(i) +'_nodes.csv'
-            adj_list = createTree.read_adj_list(path)
+            # calculate time in sequential
+            seq_start_time = round(time.time(), 4)  # time in seconds
+            sequential_dfs.dfs(adj_list, 0)
+            sequential_time.append(round(time.time(), 4) - seq_start_time)
 
-            #calculate time in sequential
-            seq_start_time = round(time.time() * 1000, 4) #time in milliseconds
-            consDFS.dfs(adj_list, 0)
-            sequential_time.append(round(time.time() * 1000, 4) - seq_start_time)
+            # calculate time in parallel
+            par_start_time = round(time.time(), 4)  # time in seconds
+            parallel_dfs.parallel_dfs(adj_list, 0, num_branches)
+            parallel_time.append(round(time.time(), 4) - par_start_time)
 
-            #calculate time in parallel
-            par_start_time = round(time.time() * 1000, 4)
-            #parallelDFS.parallel_dfs(adj_list, 0)
-            par_dfs.parallel_dfs(adj_list, 0, 4)
-            #dfs_balanced.parallel_dfs(adj_list, 4)
-            parallel_time.append(round(time.time() * 1000, 4) - par_start_time)
-            #parallel_time.append(1)
-
-        #save time results in excel file
+        # save time results in excel file
         sequential, parallel = time_results(sequential_time, parallel_time)
 
-        df = df.append({'# Nodes': i, 'Sequential time': sequential, 'Parallel time': parallel, 'Comparison':sequential/parallel}, ignore_index=True)
+        df = df.append({'# Nodes': i, 'Sequential time': sequential, 'Parallel time': parallel,
+                        'Comparison': sequential / parallel}, ignore_index=True)
         print(df)
 
     df.to_excel(excel_path, index=False)
-
-
-    # for i in number:
-    #     adj_list = createTree.create_random_tree(i)
-
-    #
-    # arr = [i for i in range(len(adj_list))]
-    # random_int = random.choice(arr)
-    # print(random_int)
-
-    # visited, visited_order = consDFS.dfs(adj_list, 0)
-    # print('Adjacency list:')
-    # print(adj_list)
-    # print('Visited:')
-    # print(visited)
-    # print('Order of visit: ')
-    # print(visited_order)
-
-
-
