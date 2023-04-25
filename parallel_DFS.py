619aac4d7dc8832d94f3299c5cc7e1c730cd9e27
Maryna Tsuk <73252923+marynatsuk@users.noreply.github.com>
2023-04-25 16:35:12 +0300

    Update parallel_DFS.py

--- parallel_DFS.py
+++ parallel_DFS.py
@@ -1,6 +1,8 @@
+import time
 import multiprocessing
 
-def cons_dfs(adj_list, start_node, visited):
+
+def sequential_dfs(adj_list, start_node, visited):
     stack = [start_node]
 
     while stack:
@@ -10,6 +12,8 @@ def cons_dfs(adj_list, start_node, visited):
             for neighbour in adj_list[current_node]:
                 if neighbour not in visited:
                     stack.append(neighbour)
+                    time.sleep(0.1)
+
     return visited
 
 
@@ -20,28 +24,14 @@ def init_pool(adj_list, num_processes, visited):
     VISITED = visited
 
 
-def divide_list(adj_list, num_parts):
-    my_tree = [[neighbour for neighbour in adj_list[node]] for node in adj_list]
-    quotient = len(adj_list) // num_parts
-    remainder = len(adj_list) % num_parts
-    sublists = []
-    start = 0
-    for i in range(num_parts):
-        end = start + quotient + (i < remainder)
-        sublists.append(my_tree[start:end])
-        start = end
-    #sublists = [my_tree[i::num_parts] for i in range(num_parts)]
-    return sublists
-
-
 def parallel_dfs(adj_list, start_node, num_processes):
     visited = multiprocessing.Array('i', len(adj_list), lock=False)
-    sub_lists = divide_list(adj_list, num_processes)
 
-    with multiprocessing.Pool(initializer=init_pool, initargs=(adj_list, num_processes, visited)) as pool:
+    with multiprocessing.Pool(initializer=init_pool,
+                              initargs=(adj_list, num_processes, visited)) as pool:
         results = []
-        for sub_adj_list in sub_lists:
-            result = pool.apply_async(cons_dfs, args=(sub_adj_list, start_node))
+        for i in range(num_processes):
+            result = pool.apply_async(sequential_dfs, args=(adj_list, i))
             results.append(result)
         for result in results:
             result.wait()
